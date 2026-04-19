6f5ca6f03e464fe8229cc999bef263377ccbb75f
Jude <drury_yl@yahoo.com>
2026-04-20 01:23:18 +0700

    Sync SalesPerson sekarang include Email-nya

--- j07-btrade-sync/Model/SalesPersonType.cs
+++ j07-btrade-sync/Model/SalesPersonType.cs
@@ -8,16 +8,18 @@ namespace j07_btrade_sync.Model
 {
     public class SalesPersonType
     {
-        public SalesPersonType(string salesPersonId, string salesPersonCode, string salesPersonName, string serverId)
+        public SalesPersonType(string salesPersonId, string salesPersonCode, string salesPersonName, string email, string serverId)
         {
             SalesPersonId = salesPersonId;
             SalesPersonCode = salesPersonCode;
             SalesPersonName = salesPersonName;
+            Email = email;
             ServerId = serverId;
         }
         public string SalesPersonId { get; set; }
         public string SalesPersonCode { get; set; }
         public string SalesPersonName { get; set; }
+        public string Email { get; set; }
         public string ServerId { get; set; }
     }
 }
